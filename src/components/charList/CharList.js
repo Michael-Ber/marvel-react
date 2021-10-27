14dca3b433eae82ba1962641ee7f88f59df2af5d
Bershadsky Mihail Sergeevich <Michail.Bershadsky@ilimtimber.corp>
2021-10-27 16:37:20 +0800

    on lesson 119

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -9,13 +9,24 @@ class CharList extends React.Component {
     marvelService = new MarvelService();
     componentDidMount() {
         this.onUpdateChars();
-        
+        // window.addEventListener('scroll', this.onUpdateScroll);
     }
+    // componentWillUnmount () {
+    //     window.removeEventListener('scroll', this.onUpdateScroll);
+    // }
+    // onUpdateScroll = () => {
+    //     console.log(window.pageYOffset, document.documentElement.scrollHeight, document.documentElement.clientHeight);
+    //     if((window.pageYOffset + document.documentElement.clientHeight) >= document.documentElement.scrollHeight) {
+    //         this.onUpdateMore();
+    //     }
+    // }
     state = {
         chars: [],
         loading: true,
         error: false,
-        moreCounter: 219
+        loadingMore: false,
+        charsEnded: false,
+        moreCounter: 1530
     };
     onUpdateChars = () => {
         this.onLoading();
@@ -27,28 +38,46 @@ class CharList extends React.Component {
         this.setState({chars, loading: false})
     }
     onMoreCharsLoaded = (more) => {
-            this.setState(({chars, moreCounter}) => {
-                return {
-                    chars: [...chars, ...more],
-                    loading: false,
-                    moreCounter: moreCounter + 9
-                }
-            })
+        let ended = false;
+        if(more.length < 9) {
+            ended = true;
+        }
+        this.setState(({chars, moreCounter}) => {
+            return {
+                chars: [...chars, ...more],
+                charsEnded: ended,
+                moreCounter: moreCounter + 9
+            }
+        })
     }
     onLoading = () => {
         this.setState({loading: true});
     }
+    onLoadingMore = () => {
+        this.setState({loadingMore: true})
+    }
     onError = () => {
         this.setState({loading: false, error: true});
     }
-    onLoadingMore = () => {
-        this.onLoading();
+    onUpdateMore = (e) => {
+        this.onLoadingMore();
+        if(e) {
+            e.target.style.display = 'none';
+        }
         this.marvelService.getAllCharacters(this.state.moreCounter)
             .then(this.onMoreCharsLoaded)
             .catch(this.onError)
+            .finally(() => {
+                this.setState({loadingMore: false});
+                if(this.state.charsEnded && e) {
+                    e.target.style.display = 'none';
+                }else if(!this.state.charsEnded && e){
+                    e.target.style.display = 'block'; 
+                }
+            })
     }
     render() {
-        const {chars, loading, error} = this.state;
+        const {chars, loading, error, loadingMore} = this.state;
         const loadingMessage = loading ? <div style={{gridRowStart: 2, gridColumnStart: 2,  justifySelf: 'center', alignSelf: 'center'}}> <Spinner /> </div> : null;
         const errorMessage = error ?<div style={{gridRowStart: 1, gridColumnStart: 2,   justifySelf: 'center', alignSelf: 'center'}}><ErrorMessage /></div> : null;
         const elems = !(error) ? chars.map(item => {
@@ -71,19 +100,20 @@ class CharList extends React.Component {
                 </li>
             )
         }): null; 
+        const spinnerInsteadBtn = loadingMore ? <div style={{marginTop: '45px'}}><Spinner /></div>: null;
         return (
             <div className="char-content">
                 <ul className="char-content__list">
                     {loadingMessage}
                     {errorMessage}
                     {elems}
-                    
                 </ul>
+                {spinnerInsteadBtn}
                 <button 
-                        className="char-content-btn btn btn-main btn-long"
-                        onClick = {this.onLoadingMore}
-                        disabled={this.state.loading}>Load more
-                    </button>
+                    className="char-content-btn btn btn-main btn-long"
+                    onClick = {(e) => this.onUpdateMore(e)}
+                    disabled={this.state.loadingMore}>Load more
+                </button>
             </div>
         )
     }
